ea24a4fbe6893dcc65345a71af839446aa56e05e
ericmartineau <smartytime@gmail.com>
2021-03-22 08:10:38 -0700

    Updating dependencies, adding global getter for sunnyLocalization

--- lib/sunny_localization.dart
+++ lib/sunny_localization.dart
@@ -1,5 +1,11 @@
 import 'package:timezone/timezone.dart';
 
+import 'sunny_get.dart';
+
+/// Global getter for the sunny localization object.  Requires that the
+/// instance has been registered with a SunnyGet container somewhere.
+SunnyLocalization get sunnyLocalization => sunny.get();
+
 /// Based on the timezone package - provides the user's timezone and location (not GPS, just generalized)
 class SunnyLocalization {
   TimeZone? userTimeZone;
